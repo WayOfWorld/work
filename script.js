2c8f2f2ab8e4de70e5dc3c6fc3269cdda9d709a7
World <1137532098@qq.com>
2025-09-27 22:45:21 +0800

    v1.0 移除了大量废弃代码,界面全面更新,元素优化, 修复了部分界面异常,界面更新(菜单,配色), 概况功能完成,简化配色,添加概况滚动条,移除了旧版彩蛋 实时滚动条选中效果,菜单功能及内容完成,添加了彩蛋

--- script.js
+++ script.js
@@ -67,6 +67,23 @@ document.querySelector('#todo_number_warn').textContent = n_warn;
 let overview_bar = document.querySelector('.overview_bar');
 let text_infos = document.querySelectorAll('.text_info_each:not([hidden])');
 
+/**
+    @param {element} bar_part
+*/
+function intersection_callback (bar_part) {
+    /**
+    @param {IntersectionObserverEntry[]} entries
+    */
+    function func(entries) {
+        if (entries[0].isIntersecting)
+            bar_part.setAttribute('selected','');
+        else
+            bar_part.removeAttribute('selected');
+    }
+
+    return func;
+}
+
 text_infos.forEach(element => {
     let bar_part = document.createElement('div');
     
@@ -75,13 +92,15 @@ text_infos.forEach(element => {
     if (each_deadline)
         bar_part.style.backgroundColor = window.getComputedStyle(each_deadline).color;
 
+    //概况条选中
+    const observer = new IntersectionObserver(intersection_callback(bar_part),{threshold:1});
+    observer.observe(element);
+
     overview_bar.appendChild(bar_part);
 })
 
 //种数统计
 document.querySelector('#todo_number_total').textContent = text_infos.length;
-
-
 
 //展开(新按钮)
 let fold_dis = document.querySelectorAll('.fold_dis');
@@ -108,7 +127,6 @@ fold_dis.forEach(element => {
 
     });
 });
-
 
 //展开(日志)
 let fold_log = document.querySelector('.news_button');
@@ -148,3 +166,51 @@ let info_view_last = [document.querySelector('meta[name="version"]').getAttribut
 if (localStorage.getItem('view_last') != info_view_last) {
     localStorage.setItem('view_last',info_view_last);
 }
+
+// 菜单选中
+let menu_info = document.querySelector('.menu_info');
+
+let menu_funcs = menu_info.querySelectorAll('.func');
+let menu_selected;
+
+let egg_loc = document.querySelector('#egg_loc');
+let egg_html = '<iframe style="width: 100%; height: 100%;" src="//player.bilibili.com/player.html?isOutside=true&aid=80433022&bvid=BV1GJ411x7h7&cid=137649199&p=1&danmaku=0&autoplay=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="false"></iframe>'
+
+function menu_select(id) {
+    menu_info.querySelectorAll('.menu_info_each').forEach(element => element.hidden = true);
+
+    egg_loc.innerHTML = '';
+
+    if (id) {
+        if (id == menu_selected) {
+            menu_select(null);
+            return;
+        }
+
+        if (id == 'egg')
+            egg_loc.innerHTML = egg_html;
+
+        menu_info.setAttribute('selected','');
+        menu_info.querySelector(`#${id}`).hidden = false;
+        menu_selected = id;
+    } else {
+        menu_info.removeAttribute('selected');
+        if (menu_selected)
+            menu_info.querySelector(`#${menu_selected}`).hidden = false;
+        menu_selected = null;
+    }
+
+    menu_funcs.forEach(element => {
+        if (id && element.getAttribute('link') == id)
+            element.setAttribute('selected','');
+        else
+            element.removeAttribute('selected');
+    });
+}
+menu_select(null);
+
+menu_info.querySelectorAll('.func').forEach(element => {
+    element.addEventListener('click',event => {
+        menu_select(element.getAttribute('link'));
+    })
+});
